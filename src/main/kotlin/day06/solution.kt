dccc841a7da2b6dfb7dbd5d2959be6a7c8fe1202
Kubo <jsodpad@gmail.com>
2022-12-06 08:30:16 +0100

    Day 06 Pimp

--- src/main/kotlin/day06/solution.kt
+++ src/main/kotlin/day06/solution.kt
@@ -6,13 +6,13 @@ fun main() {
 
     val signal = File("src/main/kotlin/day06/input.txt").readLines().first()
 
-    val packetMarkerPosition = signal.windowed(4).withIndex().first {
-        it.value.toList().size == it.value.toSet().size
-    }.index + 4
+    val packetMarkerPosition = signal.findMarker(4)
     println(packetMarkerPosition)
 
-    val messageMarkerPosition = signal.windowed(14).withIndex().first {
-        it.value.toList().size == it.value.toSet().size
-    }.index + 14
+    val messageMarkerPosition = signal.findMarker(14)
     println(messageMarkerPosition)
 }
+
+private fun String.findMarker(packetLength: Int) = this.windowed(packetLength).withIndex().first {
+    it.value.toList().size == it.value.toSet().size
+}.index + packetLength
